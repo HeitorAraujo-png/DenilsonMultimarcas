e3f594a2e33b77c5b89b11f41399fb807dc9c7bc
Heitor Araujo <213344327+HeitorAraujo-png@users.noreply.github.com>
2025-11-18 21:18:59 -0400

    Arrumando as importacoes

--- admin_core/urls.py
+++ admin_core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from admin_core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.admin_view, name='home_adm')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- login_core/urls.py
+++ login_core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from login_core import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.login_view, name='login'),
@@ -11,3 +12,6 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
