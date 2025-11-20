a30d6014a2dadbc04d88c9fd72666f2b4f5a63bd
Heitor Araujo <213344327+HeitorAraujo-png@users.noreply.github.com>
2025-11-20 17:20:06 -0400

    Iniciando a home de admin

--- admin_core/urls.py
+++ admin_core/urls.py
@@ -4,8 +4,35 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.admin_view, name='home_adm')
+    path('', views.admin_view, name='home_adm'),
+    path('view-clothes/', views.view_clothes, name='home_adm'),
+    path('view-users/', views.view_users, name='home_adm'),
+    path('create-clothe/', views.create_clothe, name='home_adm'),
+    path('create-category/', views.create_category, name='home_adm'),
+    path('create-img-clothe/', views.create_img_clothe, name='home_adm'),
+    path('create-user/', views.create_user, name='home_adm'),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- admin_core/views.py
+++ admin_core/views.py
@@ -4,3 +4,37 @@ from core.decorators import admin_required
 @admin_required
 def admin_view(request):
     return render(request, 'admin_core/admin.html')
+
+# Crud
+@admin_required
+def create_clothe(request):
+    return render(request, 'admin_core/create/clothe.html')
+
+@admin_required
+def create_category(request):    
+    return render(request, 'admin_core/create/category.html')
+
+@admin_required
+def create_img_clothe(request):    
+    return render(request, 'admin_core/create/img_clothe.html')
+
+@admin_required
+def create_user(request):    
+    return render(request, 'admin_core/create/user.html')
+
+# cRud
+@admin_required
+def view_clothes(request):
+    return render(request, 'admin_core/view_clothes.html')
+
+@admin_required
+def view_users(request):    
+    return render(request, 'admin_core/view_users.html')
+
+# crUd
+
+
+
+
+
+# cruD
